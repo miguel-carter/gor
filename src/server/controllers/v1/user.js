88ff1260029c0ab1e524df910fd4d871cf110942
Miguel Carter <nullcarter@gmail.com>
2022-10-19 08:48:14 -0500

    initial routes

---
+++ src/server/controllers/v1/user.js
@@ -0,0 +1,14 @@
+const profile = (req, res, next) => {};
+
+const login = (req, res, next) => {};
+
+const logout = (req, res, next) => {};
+
+const signup = (req, res, next) => {};
+
+export default {
+  profile,
+  login,
+  logout,
+  signup,
+};
--- src/server/routes/v1/index.js
+++ src/server/routes/v1/index.js
@@ -1,5 +1,10 @@
 import express from "express";
-import user from "./user.js";
+import userController from "../../controllers/v1/user.js";
+
+const user = express.Router();
+user.get("/user", userController.profile);
+user.post("/user/signup", userController.signup);
+user.put("/user/login", userController.login);
 
 const v1 = express.Router();
 v1.use("/v1", user);
